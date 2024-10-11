747cd4f761e2b84709e9293b5b03786e761bcf2e
Bhavya Mittal <bhavyamittal575@gmail.com>
2024-10-11 09:51:47 +0530

    taggit

--- Blog/models.py
+++ Blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from taggit.managers import TaggableManager  # Import TaggableManager
 
 # Employee model
 class Employee(models.Model):
@@ -7,15 +8,20 @@ class Employee(models.Model):
     emp_dob = models.DateTimeField()
     emp_address = models.CharField(max_length=200)
 
-# Book model
+# Book model with tagging functionality
 class Book(models.Model):
     book_id = models.AutoField(primary_key=True)
-    book_name = models.CharField(max_length=100)  # Adjusted max_length as per your needs
-    
+    book_name = models.CharField(max_length=100)
+    # Adding tags to the Book model
+    tags = TaggableManager()  # This adds tagging functionality to the Book model
+
     class Meta:
         db_table = "MyBooks"
 
+    def __str__(self):
+        return self.book_name
 
+# Contact model
 class Contact(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField()
