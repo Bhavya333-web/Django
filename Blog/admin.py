e4a27000dc8fa379cf7f909e5172cbfd93e0b6a3
Bhavya Mittal <bhavyamittal575@gmail.com>
2024-10-11 09:58:05 +0530

    tag

--- Blog/admin.py
+++ Blog/admin.py
@@ -1,14 +1,15 @@
 from django.contrib import admin
-
-# Register your models here.
 from .models import Employee, Book
+
+# Register Employee model as it is
 admin.site.register(Employee)
-admin.site.register(Book)
 
-admin.site.site_header = "Welcome in Blog Application"
+# Customize admin for Book model to display tags
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('book_name',)  # Display book name in the admin list view
+    search_fields = ('book_name', 'tags__name')  # Allow searching by book name and tags
+
+admin.site.site_header = "Welcome to the Blog Application"
 admin.site.site_title = "First Blog Application"
-admin.site.index_title = "Blog Application"
-
-
-
-
+admin.site.index_title = "Blog Application Admin Panel"
