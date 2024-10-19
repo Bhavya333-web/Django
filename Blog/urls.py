e9141bf0ae1eb9bcb478ff9de05158e842cf8aa1
Bhavya Mittal <bhavyamittal575@gmail.com>
2024-10-19 19:36:18 +0530

    django

--- Blog/urls.py
+++ Blog/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('contact/', views.contact_view, name='contact'),  # Contact page
     path('app/book/new/', views.book_create_edit, name='book_create_edit'),  # URL for creating a new book
     path('app/book/edit/<int:book_id>/', views.book_create_edit, name='book_edit'),  # URL for editing an existing book
+    path('edit/<int:pk>/', views.update_blog, name='update_blog'),
+path('delete/<int:pk>/', views.delete_blog, name='delete_blog'),
 ]
