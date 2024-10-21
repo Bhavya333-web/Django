c02a6dc72eab02d6196062f05f9c9ea33ccbaf83
Bhavya Mittal <bhavyamittal575@gmail.com>
2024-10-21 09:39:48 +0530

    django

--- Blogapp/settings.py
+++ Blogapp/settings.py
@@ -15,6 +15,12 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'bhavyamittal575@gmail.com' # Your email address
+EMAIL_HOST_PASSWORD = 'hlosejggtncnqbfu'  
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -134,10 +140,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # settings.py
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'bhavyamittal575@gmail.com' # Your email address
-EMAIL_HOST_PASSWORD = 'hlosejggtncnqbfu' 
